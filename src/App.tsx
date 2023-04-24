f2253130e65ea02fa9f9f5a4a3d267c4a520f30e
adenugbamichael <adenugbamicke@gmail.com>
2023-04-24 17:54:40 +0100

    Highlight the selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -24,13 +24,16 @@ function App() {
       </GridItem>
       <Show above='lg'>
         <GridItem area='aside' paddingX={5}>
-          <GenreList onSelectGenre={genre => setSelectedGenre(genre)} />
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectGenre={genre => setSelectedGenre(genre)}
+          />
         </GridItem>
       </Show>
       <GridItem area='main'>
         <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
-    </Grid>   
+    </Grid>
   )
 }
 
