c04636c49fe5f59973a628931d9021ad9ea6f394
adenugbamichael <adenugbamicke@gmail.com>
2023-04-24 06:04:09 +0100

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,16 @@ import { useEffect, useState } from "react"
 import apiClients from "../services/api-clients"
 import { CanceledError } from "axios"
 
+export interface Platfrom {
+  id: number
+  name: string
+  slug: string
+}
 export interface Game {
   id: number
   name: string
   background_image: string
+  parent_platforms: { platform: Platfrom }[]
 }
 
 interface FetchGamesResponse {
