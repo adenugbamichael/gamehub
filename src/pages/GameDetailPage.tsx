85bba9073ed7bb3e86023a0160be0e534f5ef952
adenugbamichael <adenugbamicke@gmail.com>
2023-04-28 12:45:41 +0100

    Improve the layout of game detail page

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react"
+import { Heading, GridItem, SimpleGrid, Spinner, Text } from "@chakra-ui/react"
 import React from "react"
 import { useParams } from "react-router-dom"
 import CriticScore from "../components/CriticScore"
@@ -18,13 +18,17 @@ const GameDetailPage = () => {
   if (error || !game) throw error
 
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameScreenshots gameId={game.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id} />
+        <GameScreenshots gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   )
 }
 
